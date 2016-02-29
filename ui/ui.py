5357550789f032adea5ee6f44372421982cab937
aklein00 <aklein00@gmail.com>
2016-02-29 08:29:15 -0800

    ui.py changes
    these were changed previously to add a quick 'Circle' Test'

--- ui/ui.py
+++ ui/ui.py
@@ -3,7 +3,12 @@ import maya.cmds as cmds
 print "This is UI"
 
 def rigarm(*args):
-	print "Rig_Arm test again"
+	print "Rig_Arm. This is a test. Files in Python_101_S1_2016 folder"
+
+
+def testbutton(*args):
+	cmds.circle( n='ikh_ctrl_arm', normal=(1, 0, 0), radius=1.5)
+	cmds.group('ikh_ctrl_arm', n='grp_ikh_ctrl_arm', world=True)
 
 
 # The UI class
@@ -15,6 +20,7 @@ class RDojo_UI:
 
 mymenu = cmds.menu( 'RDojo_Menu', label='RD Menu', to=True, p='MayaWindow')
 cmds.menuItem(label='Rig_Arm', p=mymenu, command=rigarm)
+cmds.menuItem(label='Circle Test', p=mymenu, command=testbutton)
 
 
 
