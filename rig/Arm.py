00aa503e9fc31196fb5e2a9e27f9eb9af20144cd
JordiBalfego <jordi.balfego@gmail.com>
2016-03-08 23:54:04 +0100

    Week 5 and a half

--- rig/Arm.py
+++ rig/Arm.py
@@ -3,6 +3,7 @@ import maya.cmds as cmds
 class Arm:
 
 	def __init__(self, jntPositions, jointNames):
+		self.side = 'right'
 		self.jntPos = jntPositions
 		self.names = jointNames
 		self.jntChains = (self.names['ikPrefix'], self.names['fkPrefix'], self.names['bindPrefix']) 
@@ -14,7 +15,7 @@ class Arm:
 		for label in keys:
 			tmpJntList = []
 			for i in range(len(values)):
-				newName = label + values[i][0] + self.names['jointSufix']
+				newName = label + self.names[self.side] + values[i][0] + self.names['jointSufix']
 				tmpJntList.append([newName, values[i][1]])
 			dict[label] = tmpJntList
 		return dict
@@ -31,23 +32,21 @@ class Arm:
 				cmds.joint(item[i][0], edit=True, zso=True, oj='xyz', sao='yup')
 
 	def createControl(self, jointName):
-		# If joint is end joint, we don't want a control there
-		if 'End' not in jointName:
-			# Get control info
-			grpPos = cmds.xform(jointName, q=True, t=True, ws=True)
-			grpName = self.names['groupPrefix'] + self.names['controlPrefix'] + jointName[:-len(self.names['jointSufix'])]
-			grpRot = cmds.xform(jointName, q=True, ro=True, ws=True)
-			ctrlName = self.names['controlPrefix'] + jointName[:-len(self.names['jointSufix'])]
-			# Create and orient control and control group
-			cmds.group(em=True, name=grpName)
-			cmds.xform(grpName, ro=grpRot, ws=True)
-			cmds.circle(n=ctrlName, nr=(1, 0, 0), c=(0, 0, 0))
-			cmds.delete(ctrlName, constructionHistory=True)
-			cmds.parent(ctrlName, grpName)
-			cmds.setAttr(ctrlName + '.rotateY', 0)
-			cmds.xform(grpName, t=grpPos, ws=True)
-			cmds.orientConstraint(ctrlName, jointName)
-			return ctrlName
+		# Get control info
+		grpPos = cmds.xform(jointName, q=True, t=True, ws=True)
+		grpName = self.names['groupPrefix'] + self.names['controlPrefix'] + jointName[:-len(self.names['jointSufix'])]
+		grpRot = cmds.xform(jointName, q=True, ro=True, ws=True)
+		ctrlName = self.names['controlPrefix'] + jointName[:-len(self.names['jointSufix'])]
+		# Create and orient control and control group
+		cmds.group(em=True, name=grpName)
+		cmds.xform(grpName, ro=grpRot, ws=True)
+		cmds.circle(n=ctrlName, nr=(1, 0, 0), c=(0, 0, 0))
+		cmds.delete(ctrlName, constructionHistory=True)
+		cmds.parent(ctrlName, grpName)
+		cmds.setAttr(ctrlName + '.rotateY', 0)
+		cmds.xform(grpName, t=grpPos, ws=True)
+		cmds.orientConstraint(ctrlName, jointName)
+		return ctrlName
 
 	def createIK(self, jntStart, jntEnd, ikHandleName):
 		# Get control info
@@ -68,7 +67,7 @@ class Arm:
 			cmds.setAttr(controlName + attr, edit=True,  lock=True, keyable=False, channelBox=False)    
 	
 	def createPV(self, ikHandleName, jointName):
-		PVName = self.names['ikPrefix'] + self.names['poleVector'] + self.names['armSufix']
+		PVName = self.names['ikPrefix'] + self.names['poleVector'] + self.names[self.side] + self.names['armSufix']
 		pos = cmds.xform(jointName, q=True, t=True, ws=True)
 		pos[0] -= 2
 		cmds.spaceLocator(n=PVName)
@@ -80,15 +79,17 @@ class Arm:
 		for key, value in dict.iteritems():
 			for i in range(len(value)):
 				jntName = value[i][0]
-				if key.startswith('ik'):
-					jntBindName = jntName.replace(self.names['ikPrefix'], self.names['bindPrefix'])
-					cmds.parentConstraint(jntName, jntBindName, weight = 0)
-				elif key.startswith('fk'):
-					jntBindName = jntName.replace(self.names['fkPrefix'], self.names['bindPrefix'])
-					cmds.parentConstraint(jntName, jntBindName)	
+				# If joint is end joint, we don't want to parent it
+				if 'End' not in jntName:
+					if key.startswith('ik'):
+						jntBindName = jntName.replace(self.names['ikPrefix'], self.names['bindPrefix'])
+						cmds.parentConstraint(jntName, jntBindName, weight = 0)
+					elif key.startswith('fk'):
+						jntBindName = jntName.replace(self.names['fkPrefix'], self.names['bindPrefix'])
+						cmds.parentConstraint(jntName, jntBindName)	
 	
 	def createArmFKIKBlend(self, dict):
-		ctrlName = self.names['controlPrefix'] + self.names['armSufix']
+		ctrlName = self.names['controlPrefix'] + self.names[self.side] + self.names['armSufix']
 		# Create blend attribute
 		cmds.select(d=True)
 		cmds.select(ctrlName)
@@ -106,12 +107,21 @@ class Arm:
 		for key, value in dict.iteritems():
 			for i in range(len(value)):
 				jntName = value[i][0]
-				if key.startswith('ik'):
-					jntBindName = jntName.replace(self.names['ikPrefix'], self.names['bindPrefix'])
-					cmds.connectAttr(multDivNode + '.output.outputX', jntBindName + '_parentConstraint1.' + jntName + 'W0')
-				elif key.startswith('fk'):
-					jntBindName = jntName.replace(self.names['fkPrefix'], self.names['bindPrefix'])
-					cmds.connectAttr(plusMinusNode + '.output1D', jntBindName + '_parentConstraint1.' + jntName + 'W1')		
+				if 'End' not in jntName:
+					if key.startswith('ik'):
+						jntBindName = jntName.replace(self.names['ikPrefix'], self.names['bindPrefix'])
+						cmds.connectAttr(multDivNode + '.output.outputX', jntBindName + '_parentConstraint1.' + jntName + 'W0')
+					elif key.startswith('fk'):
+						jntBindName = jntName.replace(self.names['fkPrefix'], self.names['bindPrefix'])
+						cmds.connectAttr(plusMinusNode + '.output1D', jntBindName + '_parentConstraint1.' + jntName + 'W1')		
+
+	def mirrorArm(self):
+		# Prepare info for the mirroring
+		self.jntPos = self.jntPos
+		self.side = 'left'
+		self.jntInfo = self.createDict(self.jntChains, self.jntPos)
+		# Execute rig function
+		self.rigArm();
 
 	def rigArm(self):
 		# Create and orient joints
@@ -121,38 +131,39 @@ class Arm:
 			# Create controls and groups
 			if key.startswith('fk'):
 				for i in range(len(value)):
-					# Create and align control and group
-					ctrlName = self.createControl(value[i][0])
-					if ctrlName:
-						# Lock and hide ctrl attributes
-						tmpAttrList = ('.tx', '.ty','.tz','.sx','.sy','.sz')
-						self.lockAndHide(tmpAttrList, ctrlName)
+					if 'End' not in value[i][0]:
+						# Create and align control and group
+						ctrlName = self.createControl(value[i][0])
+						if ctrlName:
+							# Lock and hide ctrl attributes
+							tmpAttrList = ('.tx', '.ty','.tz','.sx','.sy','.sz')
+							self.lockAndHide(tmpAttrList, ctrlName)
 				# Parent cntrls
-				cmds.parent(self.names['groupPrefix'] + self.names['controlPrefix'] + self.names['fkPrefix'] + self.names['elbow'], self.names['controlPrefix'] + self.names['fkPrefix'] + self.names['shoulder'])
-				cmds.parent(self.names['groupPrefix'] + self.names['controlPrefix'] + self.names['fkPrefix'] + self.names['wrist'], self.names['controlPrefix'] + self.names['fkPrefix'] + self.names['elbow'])
+				cmds.parent(self.names['groupPrefix'] + self.names['controlPrefix'] + self.names['fkPrefix'] + self.names[self.side] + self.names['elbow'], self.names['controlPrefix'] + self.names['fkPrefix'] + self.names[self.side] + self.names['shoulder'])
+				cmds.parent(self.names['groupPrefix'] + self.names['controlPrefix'] + self.names['fkPrefix'] + self.names[self.side] + self.names['wrist'], self.names['controlPrefix'] + self.names['fkPrefix'] + self.names[self.side] + self.names['elbow'])
 				# Deselect to avoid parenting
 				cmds.select(d=True)
 			elif key.startswith('ik'):
 				# Create IK control
-				ctrlName = self.createIK(self.names['ikPrefix'] + self.names['shoulder'] + self.names['jointSufix'], self.names['ikPrefix'] + self.names['wrist'] + self.names['jointSufix'], self.names['ikHandle'] + self.names['armSufix'])
+				ctrlName = self.createIK(self.names['ikPrefix'] + self.names[self.side] + self.names['shoulder'] + self.names['jointSufix'], self.names['ikPrefix'] + self.names[self.side] + self.names['wrist'] + self.names['jointSufix'], self.names['ikHandle'] + self.names[self.side] + self.names['armSufix'])
 				# Lock and hide ctrl attributes
 				tmpAttrList = ('.sx','.sy','.sz')
 				self.lockAndHide(tmpAttrList, ctrlName)
 				# Create pole vector
-				ctrlName = self.createPV(self.names['ikHandle'] + self.names['armSufix'], self.names['ikPrefix'] + self.names['elbow'] + self.names['jointSufix'])
+				ctrlName = self.createPV(self.names['ikHandle'] + self.names[self.side] + self.names['armSufix'], self.names['ikPrefix'] + self.names[self.side] + self.names['elbow'] + self.names['jointSufix'])
 				# Lock and hide ctrl attributes
 				tmpAttrList = ('.rx', '.ry','.rz','.sx','.sy','.sz')
 				self.lockAndHide(tmpAttrList, ctrlName)
 				# Deselect to avoid parenting
 				cmds.select(d=True)
 			elif key.startswith('bn'):
-				jntName = self.names['bindPrefix'] + self.names['wrist'] + self.names['jointSufix']
+				jntName = self.names['bindPrefix'] + self.names[self.side] + self.names['wrist'] + self.names['jointSufix']
 				# Create blend ctrl
 				ctrlName = self.createControl(jntName)
-				# Rename and parent control and group
-				ctrlName = cmds.rename(ctrlName, self.names['controlPrefix'] + self.names['armSufix'])
-				grpName = cmds.rename(cmds.listRelatives(ctrlName, parent=True), self.names['groupPrefix'] + self.names['armSufix'])
-				cmds.parent(grpName, jntName)
+				# Rename and parent constraint control and group
+				ctrlName = cmds.rename(ctrlName, self.names['controlPrefix'] + self.names[self.side] + self.names['armSufix'])
+				grpName = cmds.rename(cmds.listRelatives(ctrlName, parent=True), self.names['groupPrefix'] + self.names[self.side] + self.names['armSufix'])
+				cmds.parentConstraint(jntName, grpName)					
 				# Scale control
 				cmds.setAttr(grpName + '.sx', 2)
 				cmds.setAttr(grpName + '.sy', 2)
