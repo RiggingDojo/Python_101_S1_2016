1da99cf9d0e6664b295a71856961623bc8e40c52
Arthur Klein <aklein00@gmail.com>
2016-03-25 00:44:49 -0700

    Adding more buttons and fields to the UI
    trying to finish up the week6 assignment. This is not working as
    desired.

--- rdui/rdui.py
+++ rdui/rdui.py
@@ -7,6 +7,8 @@ This window has buttons to rig an arm and eventually other tools.
 import maya.cmds as cmds
 import rig.rig_arm as rig
 import system.utils as utils
+import os
+from functools import partial
 reload(utils)
 reload(rig)
 
@@ -57,35 +59,57 @@ class RDojo_UI:
             cmds.deleteUI(window_name)
 
         '''define dimensions of window and buttons'''
-        window_width = 320
+        window_width = 350
         window_height = 480
-        button_width = 100
+        button_width = 50
         button_height = 30
-        field_width = 200
+        field_width = 100
 
         self.UIElements['window'] = cmds.window(window_name, width=window_width, height=window_height, title='RDojo_UI', rtf=True)
 
         self.UIElements['mainColLayout'] = cmds.columnLayout( adjustableColumn=True )
         self.UIElements['guiFrameLayout1'] = cmds.frameLayout( label='Layout', borderStyle='out', p=self.UIElements['mainColLayout'] )
         self.UIElements['guiFlowLayout1'] = cmds.flowLayout( v=False, width=window_width, height=window_height/2, wr=True, bgc=[0.2, 0.2, 0.2], p=self.UIElements['guiFrameLayout1'])
-        #self.UIElements['rowColumnLayout1'] = cmds.rowColumnLayout( numberOfColumns=3, columnAlign=(1, 'right'), columnAttach=(2, 'both', 0), columnWidth=(2, 150) )
+        self.UIElements['rowColumnLayout1'] = cmds.rowColumnLayout( numberOfColumns=3, columnAlign=[ (1, 'right'), (2, 'right'), (3, 'right')], columnWidth=[(1, 100), (2, 150), (3, 100)] )
 
-        self.UIElements['joint1_label'] = cmds.text( label='Joint1 Name', p=self.UIElements['guiFlowLayout1'])
-        self.UIElements['enter_name'] = cmds.textField( width=field_width, p=self.UIElements['guiFlowLayout1'])
+        self.UIElements['joint1_label'] = cmds.text( label='Joint1 Name', p=self.UIElements['rowColumnLayout1'])
+        self.UIElements['enter_name'] = cmds.textField( width=field_width, p=self.UIElements['rowColumnLayout1'])
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
 
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
 
-        self.UIElements['rig_button'] = cmds.button(label='rig_arm', width=button_width, height=button_height, bgc=[0.2, 0.4, 0.2], p=self.UIElements['guiFlowLayout1'], command=self.rig_arm_full_button)
-        self.UIElements['invisbutton1'] = cmds.button(width=button_width, height=button_height, p=self.UIElements['guiFlowLayout1'], visible=False)
-        self.UIElements['invisbutton1'] = cmds.button(width=button_width, height=button_height, p=self.UIElements['guiFlowLayout1'], visible=False)
+        self.UIElements['joint1_xvalue'] = cmds.intFieldGrp( numberOfFields=3, value1=0, value2=0, value3=0, p=self.UIElements['guiFlowLayout1'])
+        #self.UIElements['separator'] = cmds.separator( h=10, style='none')
+        #self.UIElements['separator'] = cmds.separator( h=10, style='none')
 
-        self.UIElements['define_joints'] = cmds.button(label='Define Joints', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['guiFlowLayout1'], command=self.define_joints_button)
-        self.UIElements['create_arm_joints'] = cmds.button(label='Create Arm Joints', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['guiFlowLayout1'], command=self.create_joints_button)
-        self.UIElements['make_ik_controls'] = cmds.button(label='Make IK Controls', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['guiFlowLayout1'], command=self.make_ik_controls_button)
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+
+        self.UIElements['rig_button'] = cmds.button(label='rig_arm', width=button_width, height=button_height, bgc=[0.2, 0.4, 0.2], p=self.UIElements['rowColumnLayout1'], command=self.rig_arm_full_button)
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+        self.UIElements['separator'] = cmds.separator( height=10, style='none' )
+
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+
+        self.UIElements['define_joints'] = cmds.button(label='Define Joints', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['rowColumnLayout1'], command=self.define_joints_button)
+        self.UIElements['create_arm_joints'] = cmds.button(label='Create Arm Joints', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['rowColumnLayout1'], command=self.create_joints_button)
+        self.UIElements['make_ik_controls'] = cmds.button(label='Make IK Controls', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['rowColumnLayout1'], command=self.make_ik_controls_button)
         
-        self.UIElements['make_fk_controls'] = cmds.button(label='Make FK Controls', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['guiFlowLayout1'], command=self.make_fk_controls_button)
-        self.UIElements['connect_blend_nodes'] = cmds.button(label='Blend Nodes', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['guiFlowLayout1'], command=self.connect_blend_nodes_button)
-        self.UIElements['invisbutton1'] = cmds.button(width=button_width, height=button_height, p=self.UIElements['guiFlowLayout1'], visible=False)
+        self.UIElements['make_fk_controls'] = cmds.button(label='Make FK Controls', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['rowColumnLayout1'], command=self.make_fk_controls_button)
+        self.UIElements['connect_blend_nodes'] = cmds.button(label='Blend Nodes', width=button_width, height=button_height, bgc=[0.2, 0.2, 0.4], p=self.UIElements['rowColumnLayout1'], command=self.connect_blend_nodes_button)
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
         
+        self.UIElements['json_filename'] = cmds.textFieldGrp( label='Json Filename: ', text='enter filename', p=self.UIElements['guiFlowLayout1'])
+        self.UIElements['writeJson_field'] = cmds.textFieldGrp( label='Json Filename: ', text='enter data', p=self.UIElements['guiFlowLayout1'])
+        self.UIElements['separator'] = cmds.separator( h=10, style='none')
+
+        self.UIElements['writeJson_button'] = cmds.button( 'writeJson_button', command=partial(utils.writeJson, os.environ["RD_DATA"] + self.UIElements['json_filename'], self.UIElements['writeJson_field']))
+        #self.UIElements['writeJson_button'] = cmds.button( command=utils.writeJson(self.UIElements['json_filename'], self.UIElements['writeJson_field']))
 
         '''Show the window'''
         cmds.showWindow(window_name)
--- system/utils.py
+++ system/utils.py
@@ -3,13 +3,13 @@ import json
 
 
 
-def writeJson(fileName, data):
+def writeJson(fileName, data, *args):
 	with open(fileName, 'w') as outfile:
 		json.dump(data, outfile)
 	file.close(outfile)
 
 
-def readJson(fileName):
+def readJson(fileName, *args):
 	with open(fileName, 'r') as infile:
 		data = (open(infile.name, 'r').read())
 	return data
