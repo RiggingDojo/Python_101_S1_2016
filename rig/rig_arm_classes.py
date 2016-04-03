88717a1feee3044b285316c990a6a8c4ae38ecac
blam4521 <blam452@gmail.com>
2016-04-02 23:54:48 -0700

    updated rig arm clases, utils, and data

--- rig/rig_arm_classes.py
+++ rig/rig_arm_classes.py
@@ -3,6 +3,10 @@ import json
 import os
 import system.utils as utils
 
+# Class Attr
+classname = 'Rig_Arm'
+lytfile = 'arm.json'
+numjnts = 4
 
 
 class Rig_Arm:
@@ -19,19 +23,38 @@ class Rig_Arm:
 		# stores data to be modified
 		self.rig_info = {}
 
+		# Allows for seletion of new joints to get new positions from
+		if len( cmds.ls( sl=True, type='joint' )) == numjnts :
+			sel = cmds.ls( sl=True, type='joint' )
+			positions = []
+			for s in sel:
+				positions.append( cmds.xform( s, q=True, ws=True, t=True ))
+			self.rig_info['positions'] = positions
+
+		else:
+			#goes to default
+			self.rig_info['positions'] = self.module_info['positions']
+
+		self.instance = '_L_'
+		# run righ_arm function
+		self.rig_arm()
 
 
 
 	def rig_arm( self ):
+		# de select because of the check in class
+		cmds.select( d=True )
 		# Create Ik joints, passing in keys
-		self.createJoint( self.module_info['ikjnts'] )
-		cmds.select( d=True )
+		self.rig_info['ikjnts'] = utils.createJoint( self.module_info['ikjnts'] )
+		#cmds.select( d=True )
+		
 		# Create Fk joints
-		self.createJoint( self.module_info['fkjnts'] )
-		cmds.select( d=True )
+		self.rig_info['fkjnts'] = utils.createJoint( self.module_info['fkjnts'] )
+		#cmds.select( d=True )
+		
 		# Create Rig joints
-		self.createJoint( self.module_info['rigjnt'] )
-		cmds.select( d=True )		
+		self.rig_info['rigjnt'] = self.createJoint( self.module_info['rigjnt'] )
+		#cmds.select( d=True )		
 
 
 		# Create Ik Rig
@@ -39,12 +62,12 @@ class Rig_Arm:
 		#  "ikcontrols" : ["ctrl_ik_arm, ikh_arm", "ctrl_pv_arm"]
 		ikh = cmds.ikHandle( n=self.module_info["ikcontrols"][1], sj=self.module_info['ikjnts'][0], ee=self.module_info['ikjnts'][2], sol='ikRPsolver', p=2, w=1)
 		
-		self.createControl( [[self.module_info['positions'][2], self.module_info["ikcontrols"][0]]] )
+		utils.createControl( [[self.module_info['positions'][2], self.module_info["ikcontrols"][0]]] )
 
-		pvpos = self.calculatePVPosition( [self.module_info['ikjnts'][0], self.module_info['ikjnts'][1], self.module_info['ikjnts'][2]] )
+		pvpos = utils.calculatePVPosition( [self.module_info['ikjnts'][0], self.module_info['ikjnts'][1], self.module_info['ikjnts'][2]] )
 
 		pvctrlinfo = [ [pvpos, self.module_info["ikcontrols"][2]] ]
-		self.createControl(pvctrlinfo)
+		utils.createControl(pvctrlinfo)
 
 		# Parent ikh to ctrl
 		cmds.parent( self.module_info["ikcontorls"][1], self.module_info["ikcontrols"][0])
@@ -57,7 +80,7 @@ class Rig_Arm:
 
 		
 		# Create FK Rig
-		fkctrlinfo = self.createControl( [[self.module_info["positions"][2], self.module_info["fkcontrols"][2]], 
+		fkctrlinfo = utils.createControl( [[self.module_info["positions"][2], self.module_info["fkcontrols"][2]], 
 		[self.module_info["positions"][1], self.module_info["fkcontrols"][1]], 
 		[self.module_info["positions"][0], self.module_info["fkcontrols"][0]]] ) 
 
@@ -70,46 +93,4 @@ class Rig_Arm:
 
 
 
-	def createJoint( self, jntinfo ):
-		for i in range(len( jntinfo )):
-			cmds.joint( n=jntinfo[i], p=self.module_info['positions'][i] )
-		#orient joints
-		for i in range(len( jntinfo )):
-			cmds.joint(jntinfo[i], edit=True, zeroScaleOrient=True, orientJoint="xyz", secondaryAxisOrient="yup", children=True)
-
-	def createControl( self, ctrlinfo ):
-		for info in ctrlinfo:
-			# Create ik control
-			# get ws position of wrist joint
-			pos = info[0]
-			# create an empty group
-			ctrlgrp = cmds.group( em=True, name=info[2] )
-			# Create circle control object
-			ctrl = cmds.circle( n=info[1], nr=(0, 0, 1), c=(0, 0, 0) )
-			# Parent the control to the group
-			cmds.parent( ctrl, ctrlgrp )
-			# Move the group to the joint
-			cmds.xform( ctrlgrp, t=pos, ws=True ) 
-
-
-
-	def calculatePVPosition( self, jnts ):
-		#Calculates the pole vector of jnts
-		from maya import cmds, OpenMaya
-		start = cmds.xform( jnts[0], q=True, ws=True, t=True )
-		mid = cmds.xform( jnts[1], q=True, ws=True, t=True )
-		end = cmds.xform( jnts[2], q=True, ws=True, t=True )
-		startV = OpenMaya.MVector( start[0], start[1], start[2] )
-		midV = OpenMaya.MVector( mid[0], mid[1], mid[2] )
-		endV = OpenMaya.MVector( end[0], end[1], end[2] )
-		startEnd = endV - startV
-		startMid = midV -startV
-		dotP = startMid * startEnd
-		proj = float( dotP ) / float( startEnd.length() )
-		startEndN = startEnd.normal()
-		projV = startEndN * proj
-		arrowV = startMid - projV
-		arrowV*= 0.5
-		finalV = arrowV + midV
-		return ( [finalV.x, finalV.y, finalV.z] )
 
--- system/utils.py
+++ system/utils.py
@@ -2,16 +2,59 @@ import maya.cmds as cmds
 import json
 import tempfile
 
-def writeJson(fileName, data):
+def writeJson( fileName, data ):
 	''' writes a json file'''
 	
 	with open(fileName, 'w') as outfile:
-		json.dump(data, outfile)
+		json.dump( data, outfile, sort_keys = True, indent = 4, ensure_ascii=False )
 	file.close(outfile)
 
-def readJson(fileName):
+def readJson( fileName ):
 	'''reads json file'''
 
 	with open(fileName, 'r') as infile:
-		data = (open(infile.name, 'r').read())
+		data = ( open(infile.name, 'r' ).read())
 	return data
+
+def createJoint( jntinfo ):
+	for i in range(len( jntinfo )):
+		cmds.joint( n=jntinfo[i], p=self.module_info['positions'][i] )
+	#orient joints
+	for i in range(len( jntinfo )):
+		cmds.joint(jntinfo[i], edit=True, zeroScaleOrient=True, orientJoint="xyz", secondaryAxisOrient="yup", children=True)
+
+def createControl( self, ctrlinfo ):
+	for info in ctrlinfo:
+		# Create ik control
+		# get ws position of wrist joint
+		pos = info[0]
+		# create an empty group
+		ctrlgrp = cmds.group( em=True, name=info[2] )
+		# Create circle control object
+		ctrl = cmds.circle( n=info[1], nr=(0, 0, 1), c=(0, 0, 0) )
+		# Parent the control to the group
+		cmds.parent( ctrl, ctrlgrp )
+		# Move the group to the joint
+		cmds.xform( ctrlgrp, t=pos, ws=True ) 
+
+
+
+def calculatePVPosition( self, jnts ):
+	#Calculates the pole vector of jnts
+	from maya import cmds, OpenMaya
+	start = cmds.xform( jnts[0], q=True, ws=True, t=True )
+	mid = cmds.xform( jnts[1], q=True, ws=True, t=True )
+	end = cmds.xform( jnts[2], q=True, ws=True, t=True )
+	startV = OpenMaya.MVector( start[0], start[1], start[2] )
+	midV = OpenMaya.MVector( mid[0], mid[1], mid[2] )
+	endV = OpenMaya.MVector( end[0], end[1], end[2] )
+	startEnd = endV - startV
+	startMid = midV -startV
+	dotP = startMid * startEnd
+	proj = float( dotP ) / float( startEnd.length() )
+	startEndN = startEnd.normal()
+	projV = startEndN * proj
+	arrowV = startMid - projV
+	arrowV*= 0.5
+	finalV = arrowV + midV
+	return ( [finalV.x, finalV.y, finalV.z] )
